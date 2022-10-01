2d77f107b2e11ec6e748a8e09ada1db0404b516c
veeraa1270@gmail.com <veeraa1270@gmail.com>
2022-10-01 11:57:32 -0500

    styles

---
+++ home.php
@@ -0,0 +1,199 @@
+<?php include('includes/header.php'); ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>PHP CRUD MYSQL</title>
+
+  
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+    <!-- BOOTSTRAP 4 -->
+    <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">
+    <!-- FONT AWESOEM -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="stylesheet" href="./Assets/styles.css" >
+  </head>
+  <body>
+    
+  <div class="row row-cols-1 row-cols-md-3 g-4 content">
+  <div class="col colum">
+    <div class="card">
+      <img src="./Assets/img/ciudades.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Ciudades</h5>
+        <p class="card-text">esta tabla contiene informacion de la ciudades</p>
+        <a class="navbar-brand button" href="index.php">Ciudades</a>
+        </button>
+      </div>
+    </div>
+  </div>
+
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">descripcion contenido de la tabla .</p>
+        <a class="navbar-brand button" href="index.php">index</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">descripcion contenido de la tabla .</p>
+        <a class="navbar-brand button" href="index.php">index</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">descripcion contenido de la tabla .</p>
+        <a class="navbar-brand button" href="index.php">index</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">descripcion contenido de la tabla .</p>
+        <a class="navbar-brand button" href="index.php">index</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">descripcion contenido de la tabla .</p>
+        <a class="navbar-brand button" href="index.php">index</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">descripcion contenido de la tabla .</p>
+        <a class="navbar-brand button" href="index.php">index</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">descripcion contenido de la tabla .</p>
+        <a class="navbar-brand button" href="index.php">index</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">descripcion contenido de la tabla .</p>
+        <a class="navbar-brand button" href="index.php">index</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">descripcion contenido de la tabla .</p>
+        <a class="navbar-brand button" href="index.php">index</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">descripcion contenido de la tabla .</p>
+        <a class="navbar-brand button" href="index.php">index</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">descripcion contenido de la tabla .</p>
+        <a class="navbar-brand button" href="index.php">index</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">descripcion contenido de la tabla .</p>
+        <a class="navbar-brand button" href="index.php">index</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">descripcion contenido de la tabla .</p>
+        <a class="navbar-brand button" href="index.php">index</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="ciudades.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">descripcion contenido de la tabla .</p>
+        <a class="navbar-brand button" href="index.php">index</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+<?php include('includes/footer.php'); ?>
+
+
+
+ 
+    <div>
+      <a class="navbar-brand" href="home.php">PHP MySQL CRUD</a>
+      </div>
--- includes/header.php
+++ includes/header.php
@@ -13,6 +13,7 @@
 
     <nav class="navbar navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="index.php">PHP MySQL CRUD</a>
+        <a class="navbar-brand" href="home.php">HOME</a>
       </div>
     </nav>
+
