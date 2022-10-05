396b564d201e9f0a71d7198ffd1bc80395e45333
veeraa1270@gmail.com <veeraa1270@gmail.com>
2022-10-05 17:29:11 -0500

    welcome to system novio

---
+++ welcome.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="./login/Assets/styles.css" >
+  </head>
+  <body>
+    <div class="containerWelcome">
+        <div class="containerWelcome_Grid">
+            <h1>BIENVENIDO A SISTEMA NOVIO!</h1>
+            <label>base de datos</label>
+            <a class="navbar-brand button" href="./screens/escorts.php">Ingresar</a>
+        </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html>
