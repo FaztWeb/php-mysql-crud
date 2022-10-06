57dbaf6524df8747830f82fddc1e4f4cef0b336d
veeraa1270@gmail.com <veeraa1270@gmail.com>
2022-10-06 14:08:59 -0500

    styles new components

--- welcome.php
+++ welcome.php
@@ -5,14 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="./login/Assets/styles.css" >
+     <!--  ESTILOS CSS -->
+    <link rel="stylesheet" href="styles.css" >
+  <!--  TIPO GRAFIA -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dosis&display=swap" rel="stylesheet">
   </head>
   <body>
     <div class="containerWelcome">
         <div class="containerWelcome_Grid">
             <h1>BIENVENIDO A SISTEMA NOVIO!</h1>
-            <label>base de datos</label>
-            <a class="navbar-brand button" href="./screens/escorts.php">Ingresar</a>
+            <label></label>
+            <a class="navbar-brand button" href="./login/login.php">Ingresar</a>
         </div>
     </div>
     
