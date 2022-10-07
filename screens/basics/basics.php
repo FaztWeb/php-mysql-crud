fa3929972e1d162e8f216fc929e2e8793a4b6182
veeraa1270@gmail.com <veeraa1270@gmail.com>
2022-10-07 15:17:31 -0500

    styles form basics

--- screens/basics/basics.php
+++ screens/basics/basics.php
@@ -1,3 +1,6 @@
+<?php include("../../dbConnect/db.php"); ?>
+<?php include('../../includes/header.php'); ?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -7,98 +10,151 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
       <!--  ESTILOS CSS -->
       <link rel="stylesheet" href="./Assets/styles.css" >
+      <!--  TIPO GRAFIA -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dosis&display=swap" rel="stylesheet">
   </head>
   <body>
-    <div class="conainer">
+    <br>
+        <div class="title">
+            <h1>Basicos</h1>
+        </div>
+    
+    <div class="card-body conainer">
 
-    <form>
+<form>
+        <div class="container-space">
+                
+                <div class="form-group item  select ">
+                <div class="selectItems">
+                        <label for="state_id" class="control-label">Tratamiento Personal</label>
+                        <select class="form-control " id="state_id">
+                            <option value="AL">señor</option>
+                            <option value="AK">señora</option>
+                            
+                        </select> 
+                    </div> 
+                </div>
+                <div class="form-group item">
+                    <label for="street1_id" class="control-label">Nombre</label>
+                    <input type="text" class="form-control" id="street1_id" name="street1" placeholder="Nombre">
+                </div>
+                <div class="form-group item">
+                    <label for="street2_id" class="control-label">Apellidos</label>
+                    <input type="text" class="form-control" id="street2_id" name="street2" placeholder="Apellidos">
+                </div>
+                <div class="form-group item  select ">
+                <div class="selectItems">
+                        <label for="state_id" class="control-label">Acompañante</label>
+                        <select class="form-control " id="state_id">
+                            <option value="AL">e hija</option>
+                            <option value="AK">y señora</option>
+                            
+                        </select> 
+                    </div> 
+                </div>                    
+                <div class="form-group select item gridSelect"> <!-- State Button -->
+                    
+                    <div class="selectItems">
+                        <label for="state_id" class="control-label">Ciudad</label>
+                        <select class="form-control " id="state_id">
+                            <option value="AL">Alabama</option>
+                            <option value="AK">Alaska</option>
+                            <option value="AZ">Arizona</option>
+                            <option value="AR">Arkansas</option>
+                            <option value="CA">California</option>
+                            <option value="CO">Colorado</option>
+                            <option value="CT">Connecticut</option>
+                            <option value="DE">Delaware</option>
+                            <option value="DC">District Of Columbia</option>
+                            <option value="FL">Florida</option>
+                            <option value="GA">Georgia</option>
+                            <option value="HI">Hawaii</option>
+                            <option value="ID">Idaho</option>
+                            <option value="IL">Illinois</option>
+                            <option value="IN">Indiana</option>
+                        </select> 
+                    </div> 
+                    <div class="selectItems">
+                        <label for="state_id" class="control-label">Genero</label>
+                        <select class="form-control" id="state_id">
+                            <option value="AL">Masculino</option>
+                            <option value="AK">Femenino</option>
+                        </select> 
+                    </div>
+                   
+                </div>
 
-<div class="form-group"> <!-- Full Name -->
-    <label for="full_name_id" class="control-label">Full Name</label>
-    <input type="text" class="form-control" id="full_name_id" name="full_name" placeholder="John Deer">
-</div>    
+                
 
-<div class="form-group"> <!-- Street 1 -->
-    <label for="street1_id" class="control-label">Street Address 1</label>
-    <input type="text" class="form-control" id="street1_id" name="street1" placeholder="Street address, P.O. box, company name, c/o">
-</div>                    
-                        
-<div class="form-group"> <!-- Street 2 -->
-    <label for="street2_id" class="control-label">Street Address 2</label>
-    <input type="text" class="form-control" id="street2_id" name="street2" placeholder="Apartment, suite, unit, building, floor, etc.">
-</div>    
+                <div class="form-group item"> <!-- City-->
+                    <label for="city_id" class="control-label">Cargo</label>
+                    <input type="text" class="form-control" id="city_id" name="city" placeholder="Cargo">
+                </div> 
+                <div class="form-group item"> <!-- City-->
+                    <label for="city_id" class="control-label">Empresa</label>
+                    <input type="text" class="form-control" id="city_id" name="city" placeholder="Empresa">
+                </div>
+                <div class="form-group item"> <!-- City-->
+                    <label for="city_id" class="control-label">Telefono Empresa</label>
+                    <input type="text" class="form-control" id="city_id" name="city" placeholder="Telefono Empresa">
+                </div> 
+                <div class="form-group itemTwo"> <!-- City-->
+                    <label for="city_id" class="control-label">Telefono Directo</label>
+                    <input type="text" class="form-control" id="city_id" name="city" placeholder="Telefono Directo">
+                </div>      
 
-<div class="form-group"> <!-- City-->
-    <label for="city_id" class="control-label">City</label>
-    <input type="text" class="form-control" id="city_id" name="city" placeholder="Smallville">
-</div>                                    
-                        
-<div class="form-group"> <!-- State Button -->
-    <label for="state_id" class="control-label">State</label>
-    <select class="form-control" id="state_id">
-        <option value="AL">Alabama</option>
-        <option value="AK">Alaska</option>
-        <option value="AZ">Arizona</option>
-        <option value="AR">Arkansas</option>
-        <option value="CA">California</option>
-        <option value="CO">Colorado</option>
-        <option value="CT">Connecticut</option>
-        <option value="DE">Delaware</option>
-        <option value="DC">District Of Columbia</option>
-        <option value="FL">Florida</option>
-        <option value="GA">Georgia</option>
-        <option value="HI">Hawaii</option>
-        <option value="ID">Idaho</option>
-        <option value="IL">Illinois</option>
-        <option value="IN">Indiana</option>
-        <option value="IA">Iowa</option>
-        <option value="KS">Kansas</option>
-        <option value="KY">Kentucky</option>
-        <option value="LA">Louisiana</option>
-        <option value="ME">Maine</option>
-        <option value="MD">Maryland</option>
-        <option value="MA">Massachusetts</option>
-        <option value="MI">Michigan</option>
-        <option value="MN">Minnesota</option>
-        <option value="MS">Mississippi</option>
-        <option value="MO">Missouri</option>
-        <option value="MT">Montana</option>
-        <option value="NE">Nebraska</option>
-        <option value="NV">Nevada</option>
-        <option value="NH">New Hampshire</option>
-        <option value="NJ">New Jersey</option>
-        <option value="NM">New Mexico</option>
-        <option value="NY">New York</option>
-        <option value="NC">North Carolina</option>
-        <option value="ND">North Dakota</option>
-        <option value="OH">Ohio</option>
-        <option value="OK">Oklahoma</option>
-        <option value="OR">Oregon</option>
-        <option value="PA">Pennsylvania</option>
-        <option value="RI">Rhode Island</option>
-        <option value="SC">South Carolina</option>
-        <option value="SD">South Dakota</option>
-        <option value="TN">Tennessee</option>
-        <option value="TX">Texas</option>
-        <option value="UT">Utah</option>
-        <option value="VT">Vermont</option>
-        <option value="VA">Virginia</option>
-        <option value="WA">Washington</option>
-        <option value="WV">West Virginia</option>
-        <option value="WI">Wisconsin</option>
-        <option value="WY">Wyoming</option>
-    </select>                    
-</div>
+                
 
-<div class="form-group"> <!-- Zip Code-->
-    <label for="zip_id" class="control-label">Zip Code</label>
-    <input type="text" class="form-control" id="zip_id" name="zip" placeholder="#####">
-</div>        
+        </div>
+        <div class="container-space">
+                
+                <div class="form-group itemTwo"> <!-- City-->
+                    <label for="city_id" class="control-label">Correo Empresaeial</label>
+                    <input type="text" class="form-control" id="city_id" name="city" placeholder="Correo Empresaeial">
+                </div> 
+                <div class="form-group itemTwo"> <!-- City-->
+                    <label for="city_id" class="control-label">Asistente Secretaria</label>
+                    <input type="text" class="form-control" id="city_id" name="city" placeholder="Asistente Secretaria">
+                </div> 
+                <div class="form-group itemTwo"> <!-- City-->
+                    <label for="city_id" class="control-label">Direccion Empresa</label>
+                    <input type="text" class="form-control" id="city_id" name="city" placeholder="Direccion Empresa">
+                </div>
+                <div class="form-group itemTwo"> <!-- City-->
+                    <label for="city_id" class="control-label">Direccion Residencia</label>
+                    <input type="text" class="form-control" id="city_id" name="city" placeholder="Direccion Residencia">
+                </div>
+                <div class="form-group itemTwo"> <!-- City-->
+                    <label for="city_id" class="control-label">Telefono Residencia</label>
+                    <input type="text" class="form-control" id="city_id" name="city" placeholder="Telefono Residencia">
+                </div> 
+                <div class="form-group itemTwo"> <!-- City-->
+                    <label for="city_id" class="control-label">Celular Personal</label>
+                    <input type="text" class="form-control" id="city_id" name="city" placeholder="Celular Personal">
+                </div> 
+                <div class="form-group itemTwo"> <!-- City-->
+                    <label for="city_id" class="control-label">Correo Personal</label>
+                    <input type="text" class="form-control" id="city_id" name="city" placeholder="Correo Personal">
+                </div>  
+                <div class="form-group itemTwo"> <!-- City-->
+                    <label for="city_id" class="control-label">Cedula</label>
+                    <input type="text" class="form-control" id="city_id" name="city" placeholder="Cedula">
+                </div>
+                <br>
+                <div class="form-group itemTwo"> <!-- City-->
+                   
+                        <p>Click para Ver Detalle <a href="./detail_basics/detail_basics.php"><strong>Basicos</strong></a></p>
+                   
+                </div>                   
 
-<div class="form-group"> <!-- Submit Button -->
-    <button type="submit" class="btn btn-primary">Buy!</button>
-</div>     
-
+        </div>
+        <div class="button">
+        <div class="form-group item"> <!-- Submit Button -->
+                    <button type="submit" class="btn btn-dark">ENVIAR </button>
+                </div>
+        </div>
 </form>
 
     </div>
